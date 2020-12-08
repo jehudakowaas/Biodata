7b11252e32c8ac137c6759475801b50761507be9
jehudakowaas <s21910290@student.unklab.ac.id>
2020-12-08 17:51:04 +0800

    commitFinal

---
+++ index.js
@@ -0,0 +1,19 @@
+ var circle = document.getElementById('circle');
+ var upBtn = document.getElementById('upBtn');
+ var downBtn = document.getElementById('downBtn');
+ 
+ var rotateValue = circle.style.transform;
+ var rotateSum;
+
+ upBtn.onclick = function()
+ {
+     rotateSum = rotateValue + "rotate(-90deg";
+     circle.style.transform;
+     rotateValue = rotateSum;
+ }
+function hai(){
+    alert("Hai Sir, im so sorry it is so simple... i tried make the image to rotate 180degree when you click the features but it didnt work and i did search for more references on how to create javascipt but turns out it so hard and i failed. Hope you understand. Thank you Sir, Have a nice day.");
+    document.querySelector("h1").style.color = "purple";
+    
+}
+ 
